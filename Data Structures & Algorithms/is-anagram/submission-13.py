d178e4690ddff77238e35939b9656dc5a9b224e3
Jeev Sosale <165214502+jeevss131436@users.noreply.github.com>
2026-08-27 21:37:13 -0400

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sMap = {}
+        tMap = {}
+
+        for char in s:
+            if char not in sMap:
+                sMap[char] = 1
+            else:
+                sMap[char] += 1
+        
+        for char in t:
+            if char not in tMap:
+                tMap[char] = 1
+            else:
+                tMap[char] += 1
+        
+        if sMap != tMap:
+            return False
+        return True
